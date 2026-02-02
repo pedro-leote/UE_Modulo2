d76451658d77a39722d8995782aa0064c34c864f
pedro-leote <pedro_leote@eticalgarve.com>
2026-02-02 14:47:40 +0000

    Initial FIles

---
+++ Source/UE_Modulo2/Private/Interface_01.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interface_01.h"
+
+// Add default functionality here for any IInterface_01 functions that are not pure virtual.
---
+++ Source/UE_Modulo2/Public/Actor_01.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Actor_01.generated.h"
+#include "Interface_01.h"
+
+UCLASS()
+class UE_MODULO2_API AActor_01 : public AActor, public IInterface_01
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AActor_01();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+private:
+	UPROPERTY(EditAnywhere)
+	uint32 _integerVariable = 0;
+};
